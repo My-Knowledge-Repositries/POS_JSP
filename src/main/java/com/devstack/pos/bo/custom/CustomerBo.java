ff05d01803326bd084199b2675778aca9f63ad4c
oshadaera68 <mindartlk010@gmail.com>
2023-05-07 22:32:20 +0530

    updated content

--- src/main/java/com/devstack/pos/bo/custom/CustomerBo.java
+++ src/main/java/com/devstack/pos/bo/custom/CustomerBo.java
@@ -3,12 +3,13 @@ package com.devstack.pos.bo.custom;
 import com.devstack.pos.bo.SuperBo;
 import com.devstack.pos.dto.CustomerDto;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerBo extends SuperBo {
-    public void saveCustomer(CustomerDto dto);
-    public CustomerDto findCustomer(long id);
-    public void updateCustomer(CustomerDto dto);
-    public void deleteCustomer(long id);
-    public List<CustomerDto> findAllCustomer();
+    public void saveCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException;
+    public CustomerDto findCustomer(long id) throws SQLException, ClassNotFoundException;
+    public void updateCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException;
+    public void deleteCustomer(long id) throws SQLException, ClassNotFoundException;
+    public List<CustomerDto> findAllCustomer() throws SQLException, ClassNotFoundException;
 }
--- src/main/java/com/devstack/pos/bo/custom/impl/CustomerBoImpl.java
+++ src/main/java/com/devstack/pos/bo/custom/impl/CustomerBoImpl.java
@@ -1,33 +1,59 @@
 package com.devstack.pos.bo.custom.impl;
 
 import com.devstack.pos.bo.custom.CustomerBo;
+import com.devstack.pos.dao.DaoFactory;
+import com.devstack.pos.dao.custom.CustomerDao;
 import com.devstack.pos.dto.CustomerDto;
+import com.devstack.pos.entity.Customer;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerBoImpl implements CustomerBo {
+  CustomerDao customerDao = (CustomerDao) DaoFactory.getInstance().getDao(DaoFactory.DaoType.CUSTOMER);
     @Override
-    public void saveCustomer(CustomerDto dto) {
-
+    public void saveCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException {
+customerDao.save(new Customer(dto.getCustomerId(), dto.getName(), dto.getAddress(), dto.getSalary()));
     }
 
     @Override
-    public CustomerDto findCustomer(long id) {
+    public CustomerDto findCustomer(long id) throws SQLException, ClassNotFoundException {
+        Customer customer = customerDao.find(id);
+        if (customer!=null) {
+            return new CustomerDto(
+                    customer.getCustomerId(),
+                    customer.getName(),
+                    customer.getAddress(),
+                    customer.getSalary()
+            );
+
+        }
         return null;
     }
 
     @Override
-    public void updateCustomer(CustomerDto dto) {
-
+    public void updateCustomer(CustomerDto dto) throws SQLException, ClassNotFoundException {
+        customerDao.update(new Customer(dto.getCustomerId(), dto.getName(), dto.getAddress(), dto.getSalary()));
     }
 
     @Override
-    public void deleteCustomer(long id) {
-
+    public void deleteCustomer(long id) throws SQLException, ClassNotFoundException {
+customerDao.delete(id);
     }
 
     @Override
-    public List<CustomerDto> findAllCustomer() {
-        return null;
+    public List<CustomerDto> findAllCustomer() throws SQLException, ClassNotFoundException {
+        List<CustomerDto> list = new ArrayList<>();
+        for (Customer c: customerDao.findAll()
+             ) {
+            new CustomerDto(
+                    c.getCustomerId(),
+                    c.getName(),
+                    c.getAddress(),
+                    c.getSalary()
+            );
+        }
+        return list;
     }
 }
