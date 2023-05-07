39bb58ee2525d23d572d9e33a89b80a6d603b02b
oshadaera68 <mindartlk010@gmail.com>
2023-05-07 21:00:56 +0530

    updated content

---
+++ src/main/java/com/devstack/pos/dao/custom/impl/CustomerDaoImpl.java
@@ -0,0 +1,30 @@
+package com.devstack.pos.dao.custom.impl;
+
+import com.devstack.pos.dao.custom.CustomerDao;
+import com.devstack.pos.entity.Customer;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+    public void save(Customer customer)  {
+
+    }
+
+    public void update(Customer customer) {
+
+    }
+
+    public Customer find(Long id) {
+        return null;
+    }
+
+    public void delete(Long id)  {
+
+    }
+
+    public List<Customer> findAll()  {
+        return null;
+    }
+}
--- src/main/java/com/devstack/pos/entity/Customer.java
+++ src/main/java/com/devstack/pos/entity/Customer.java
@@ -2,13 +2,21 @@ package com.devstack.pos.entity;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /*Lombok*/
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 //@Data - don't use it with entity
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long customerId;
     private String name;
     private String address;
